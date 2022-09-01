44e989c34e81f028ea7c5052dc2619479e54c34b
Vishal Sengar <vishal.sengar@paytm.com>
2022-09-02 00:23:05 +0530

    Date Utility

--- app/src/main/java/com/vishal/kotlin_github/util/RepositoryUtil.kt
+++ app/src/main/java/com/vishal/kotlin_github/util/RepositoryUtil.kt
@@ -35,9 +35,33 @@ object RepositoryUtil {
 
     @RequiresApi(Build.VERSION_CODES.O)
     fun getLastUpdated(updatedAt: String): String {
+        val ch: String
+        val currentDateTime = LocalDateTime.now().toString()
 
-        val date = updatedAt.substring(0, 10)
-        val current = LocalDateTime.now()
-        return "Updated yesterday"
+        val YY = currentDateTime.substring(0, 4).toInt() - updatedAt.substring(0, 4).toInt()
+        val MM = currentDateTime.substring(5, 7).toInt() - updatedAt.substring(5, 7).toInt()
+        val DD = currentDateTime.substring(8, 10).toInt() - updatedAt.substring(8, 10).toInt()
+        val HH = currentDateTime.substring(11, 13).toInt() - updatedAt.substring(11, 13).toInt()
+        val mm = currentDateTime.substring(14, 16).toInt() - updatedAt.substring(14, 16).toInt()
+        val SS = currentDateTime.substring(17, 19).toInt() - updatedAt.substring(17, 19).toInt()
+
+        ch = if(YY > 0) "year"
+        else if(MM > 0) "month"
+        else if(DD > 0) "day"
+        else if(HH > 0) "hour"
+        else if(mm > 0) "minute"
+        else "second"
+
+        val lastUpdatedAt = when(ch){
+            "year" -> "Updated $YY $ch ago"
+            "month" -> "Updated $MM $ch ago"
+            "day" -> "Updated $DD $ch ago"
+            "hour" -> "Updated $HH $ch ago"
+            "minute" -> "Updated $mm $ch ago"
+            else -> "Updated $SS $ch ago"
+        }
+
+        return lastUpdatedAt
     }
+
 }
